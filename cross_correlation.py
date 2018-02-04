9a1d8fc625492d066def9cfc5e71b305650cdcb5
pi as Erik <erikrm@stud.ntnu.no>
2018-02-04 09:57:53 +0000

    Commit from last thursday

--- cross_correlation.py
+++ cross_correlation.py
@@ -1,4 +1,5 @@
 import operator
+import struct
 from scipy.signal import butter, lfilter
 from numpy import correlate
 
@@ -9,18 +10,17 @@ def filter(data, f1, f2, fs):
     high = f2/nyq
 
     b, a = butter(5, [low, high], btype='band')
-    y = lfilter(b, a, data)
+    y = lfilter(b, a, data)  
     print y
     return y
 
 
 
-def delay_from_corr(set1, set2, fs):    
-    #y1 = filter(set1, f1, f2, fs)
-    #y2 = filter(set2, f1, f2, fs)
+def delay_from_corr(set1, set2, fs, f1, f2):    
+    y1 = filter(set1, f1, f2, fs)
+    y2 = filter(set2, f1, f2, fs)
 
-
-    xcorr = correlate(set1, set2, "full") #absolute value?
+    xcorr = correlate(y1, y2, "full") #absolute value?
     index, value = max(enumerate(xcorr), key=operator.itemgetter(1)) 
     print xcorr
     sample_delay = (len(xcorr)+1)/2-1-index
@@ -28,8 +28,18 @@ def delay_from_corr(set1, set2, fs):
     print "Value: %d, Index: %d" % (value, index)
     print time_delay
 
+def read_from_bin(filepath):
+    raw_data = open(filepath, "rb")
+    print raw_data.read() 
+    
+read_from_bin("ADC_sampling/adcData.bin")
+
+"""
 array1 = [0, 1, 2, 3, 4]
 array2 = [1, 2, 3, 4, 0] 
 fs = 1000
-delay_from_corr(array1, array2, fs)
+f1 = 1
+f2 = 50
+delay_from_corr(array1, array2, fs, f1, f2)
 #filter(array1, 1, 100, 1000)
+"""

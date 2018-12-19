4ab8cbbb6174352eb2b37961a2ba6cab73f304f2
erikrm <erikrm@stud.ntnu.no>
2018-12-19 13:50:22 +0800

    Added the current working folder

---
+++ RFID_storage/firebase_init.py
@@ -0,0 +1,8 @@
+
+from firebase import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials-Certificate("rfid-storage-firebase-adminsdk-r4lsn-3d80ffa440.json")
+firebase_admin.initialize_app(cred)
+
+

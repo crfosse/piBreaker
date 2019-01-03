062c722929539e38a94ed6b4d5df94aa4498ca42
erik <erikrm@stud.ntnu.no>
2019-01-03 12:09:26 +0800

    Trying to add asynch properties

--- RFID_storage/main.py
+++ RFID_storage/main.py
@@ -3,6 +3,8 @@ import nfc
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://rfid-storage.firebaseio.com',None)
 
+from multiprocessing import Pool
+
 def store_item(uid):
 
 	storage_position = firebase.get("rotation position",None)
@@ -21,15 +23,33 @@ def read_id(tag):
 
     return True #Returns to the connect function and keeps listening for new cards.
 
-clf = nfc.ContactlessFrontend()
-assert clf.open('tty:AMA0:pn532') is True
-
-
-while(1): 
-	
+def tag_search():
+	print("tag_search")
+	'''
 	tag = clf.connect(rdwr={
    	 'on-connect': lambda tag:  read_id(tag)
    	 #lambda tag: False #Simple function that stops the process after the card has been found
-   	 })
+   	})
+	   '''
+	return "tag_search"
 
+def check_rotation_pos():
+	storage_position = firebase.get("rotation position",None)
+
+	return storage_position
+
+
+#init:
+clf = nfc.ContactlessFrontend()
+assert clf.open('tty:AMA0:pn532') is True
+
+#Async:
+pool = Pool()
+result1 = pool.apply_async(tag_search)
+result2 = pool.apply_async(check_rotation_pos)
+answer1 = resutl1.get(timeout=10)
+answer2 = result2.get(timeout=10)
+
+	
+#Postludium:
 clf.close()

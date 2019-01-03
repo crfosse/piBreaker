01760b760ebdf4dc1eb1a41310677167e2ded487
erik <erikrm@stud.ntnu.no>
2019-01-03 13:34:08 +0800

    Cleaned up the code

--- RFID_storage/main.py
+++ RFID_storage/main.py
@@ -1,11 +1,10 @@
-import time
-
+#import time
 import nfc
+from multiprocessing import Pool
 
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://rfid-storage.firebaseio.com',None)
 
-from multiprocessing import Pool
 
 #Firebase functions:
 def store_item(uid):
@@ -31,10 +30,9 @@ def tag_search():
 	print("tag_search")
 	tag = clf.connect(rdwr={
    	 'on-connect': lambda tag:  read_id(tag)
-   	 #lambda tag: False #Simple function that stops the process after the card has been found
    	})
 	if(tag == True):
-		tag_search()
+		tag_search() #Not exactly a pretty solution...
 
 #Stepper functions:
 def stepper_control(pos): 
@@ -43,32 +41,36 @@ def stepper_control(pos):
 def rotate_storage():
 	storage_position = firebase.get("rotation position",None)
 	
-	if(storage_position != -1):
-	     stepper_control(storage_position)
+	if(storage_position > 0 | storage_position < 5):
+	    stepper_control(storage_position)
 
 	return storage_position
+
 
 
 #init:
 clf = nfc.ContactlessFrontend()
 assert clf.open('tty:AMA0:pn532') is True
 
+
+
 #Async:
 pool = Pool()
-answer2 = 0
+storage_pos = 0
 
+#Error handling: https://jreese.sh/blog/python-multiprocessing-keyboardinterrupt 
 try:
 	pool.apply_async(tag_search)
 
-	while(answer2 != -1):
-		#result1 = pool.apply_async(tag_search)
-		result2 = pool.apply_async(rotate_storage)
+	while(storage_pos != -1):
+		
+		rotation_result = pool.apply_async(rotate_storage)
 
-		#answer1 = result1.get(timeout=10)
-		answer2 = result2.get(timeout=10)
+		rotation_answer = rotation_result.get(timeout=10)
 
-		#print answer1
-		print answer2
+		print rotation_answer
+
+	answer1 = result1.get(timeout=1) #Shutting down the tag_search in a super ugly way.
 except KeyboardInterrupt:
 	print("Caught Keyboard interrupt")
 	pool.terminate()

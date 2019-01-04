4fce13e1c2ef62a9387d2319a3bed289e2468573
erikrm <erikrm@stud.ntnu.no>
2019-01-04 12:24:54 +0800

    Working version, tested some

--- RFID_storage/main.py
+++ RFID_storage/main.py
@@ -39,8 +39,8 @@ def stepper_control(pos):
 	return True	
 
 def rotate_storage():
-	storage_position = firebase.get("rotation position",None)
-	
+	storage_position = int(firebase.get("rotation position",None))
+
 	if(storage_position > 0 | storage_position < 5):
 	    stepper_control(storage_position)
 
@@ -56,21 +56,21 @@ assert clf.open('tty:AMA0:pn532') is True
 
 #Async:
 pool = Pool()
-storage_pos = 0
+rotation_answer = 0
 
 #Error handling: https://jreese.sh/blog/python-multiprocessing-keyboardinterrupt 
 try:
-	pool.apply_async(tag_search)
+	tag_result = pool.apply_async(tag_search)
 
-	while(storage_pos != -1):
+	while(rotation_answer  != -1):
 		
 		rotation_result = pool.apply_async(rotate_storage)
 
 		rotation_answer = rotation_result.get(timeout=10)
 
 		print rotation_answer
+	pool.terminate()
 
-	answer1 = result1.get(timeout=1) #Shutting down the tag_search in a super ugly way.
 except KeyboardInterrupt:
 	print("Caught Keyboard interrupt")
 	pool.terminate()

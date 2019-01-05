c38c0da6a8e92d5383f58b8152b164c8c15226bf
erikrm <erikrm@stud.ntnu.no>
2019-01-05 12:24:56 +0800

    Working on button push, everything and the stepper now works! Everything is comming together!

--- RFID_storage/main.py
+++ RFID_storage/main.py
@@ -1,4 +1,4 @@
-#import time
+import time
 import nfc
 from multiprocessing import Pool
 
@@ -36,14 +36,8 @@ def tag_search():
 		tag_search() #Not exactly a pretty solution...
 
 #Stepper functions:
-def stepper_control(pos): 
-	return True	
-
 def rotate_storage():
-	storage_position = firebase.get("rotation position",None)
-	
-	if(storage_position > 0 | storage_position < 5):
-	    stepper_control(storage_position)
+	storage_position = firebase.get("rotation position",None)    
 
 	return storage_position
 
@@ -58,6 +52,7 @@ ser = serial.Serial('/dev/ttyACM0',9600) #Arduino communication
 #Async:
 pool = Pool()
 storage_pos = 0
+old_rotation = 1
 
 #Error handling: https://jreese.sh/blog/python-multiprocessing-keyboardinterrupt 
 try:
@@ -68,18 +63,25 @@ try:
 		rotation_result = pool.apply_async(rotate_storage)
 
 		rotation_answer = rotation_result.get(timeout=10)
-
-		ser.write(str(rotation_answer))
-		print rotation_answer
+		if(old_rotation != rotation_answer):
+			ser.write(str(rotation_answer))
+			print rotation_answer
+			old_rotation = rotation_answer
+			#time.sleep(500)
+			#print ser.readline()
 
 	answer1 = result1.get(timeout=1) #Shutting down the tag_search in a super ugly way.
 except KeyboardInterrupt:
 	print("Caught Keyboard interrupt")
 	pool.terminate()
 	pool.join()
-	clf.close()	
+	clf.close()
+	ser.write('1')
+	ser.close()	
 else: 
 	print "Quitting normaly"
 	pool.close()
 	pool.join()
 	clf.close()
+	ser.write('1')
+	ser.close()

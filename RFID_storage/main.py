3d241db84a51e6e57a321be64f0d122f7aeeeb9b
erik <erikrm@stud.ntnu.no>
2019-02-04 16:52:25 +0800

    Made a function called main, wont run automaticly now

--- RFID_storage/main.py
+++ RFID_storage/main.py
@@ -55,68 +55,73 @@ def button_insert2storage(pin_number):
 		button_pressed  = True
 	
 #init:
-
-clf = nfc.ContactlessFrontend()
-try:
-	assert clf.open('tty:AMA0:pn532') is True, "Cannot connect to nfc reader"
-except AssertionError:
-	
-
-ser = serial.Serial('/dev/ttyACM0',9600) #Arduino communication
-global button_pressed
-button_pressed = False
-
-
-#Button interrupt:
-GPIO.setmode(GPIO.BOARD) #Physical pin numbering
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-
-GPIO.add_event_detect(16, GPIO.RISING, callback=button_insert2storage, bouncetime=3000)
-
-
-#Async:
-pool = Pool()
-storage_pos = 0
-old_rotation = 1
-
-#Error handling: https://jreese.sh/blog/python-multiprocessing-keyboardinterrupt 
-try:
-	pool.apply_async(tag_search)
-
-	while(storage_pos != -1):
-		if(button_pressed == True):
-			#global button_pressed
-			button_pressed  = False
-			stor_pos = old_rotation #firebase.get("rotation position", None)
-			if(stor_pos == 4):
-				result = firebase.put("/","rotation position",1)
-			else: 
-				result = firebase.put("/","rotation position",(stor_pos + 1))	
+def main():
 		
-		rotation_result = pool.apply_async(rotate_storage)
+	clf = nfc.ContactlessFrontend()
 
-		rotation_answer = rotation_result.get(timeout=10)
-		if(old_rotation != rotation_answer):
-			ser.write(str(rotation_answer))
-			print rotation_answer
-			old_rotation = rotation_answer
-			#time.sleep(500)
-			#print ser.readline()
+	error_nfc_connection = "Cannot connect to the nfc reader, might be connected to the wrong port or the last connection was not terminated properly. If the last one happened you might have to restart the nfc reader, good old turn on and off"
+	assert clf.open('tty:AMA0:pn532') is True, error_nfc_connection
 
-	#answer1 = result1.get(timeout=1) #Shutting down the tag_search in a super ugly way.
-except KeyboardInterrupt:
-	print("Caught Keyboard interrupt")
-	pool.terminate()
-	pool.join()
-	clf.close()
-	ser.write('1')
-	ser.close()	
-	GPIO.cleanup()
-else: 
-	print "Quitting normaly"
-	pool.close()
-	pool.join()
-	clf.close()
-	ser.write('1')
-	ser.close()
-	GPIO.cleanup()
+	error_arduino_connection = "Cannot connect to Arduino, probably trying to connect trough the wrong port"
+	try:
+		ser = serial.Serial('/dev/ttyACM0',9600) #Arduino communication
+	except Exception:
+		print error_arduino_connection
+		clf.close()
+
+	global button_pressed
+	button_pressed = False
+
+	#Button interrupt:
+	GPIO.setmode(GPIO.BOARD) #Physical pin numbering
+	GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
+
+	GPIO.add_event_detect(16, GPIO.RISING, callback=button_insert2storage, bouncetime=3000)
+
+
+	#Async:
+	pool = Pool()
+	storage_pos = 0
+	old_rotation = 1
+
+	#Error handling: https://jreese.sh/blog/python-multiprocessing-keyboardinterrupt 
+	try:
+		pool.apply_async(tag_search)
+
+		while(storage_pos != -1):
+			if(button_pressed == True):
+				#global button_pressed
+				button_pressed  = False
+				stor_pos = old_rotation #firebase.get("rotation position", None)
+				if(stor_pos == 4):
+					result = firebase.put("/","rotation position",1)
+				else: 
+					result = firebase.put("/","rotation position",(stor_pos + 1))	
+			
+			rotation_result = pool.apply_async(rotate_storage)
+
+			rotation_answer = rotation_result.get(timeout=10)
+			if(old_rotation != rotation_answer):
+				ser.write(str(rotation_answer))
+				print rotation_answer
+				old_rotation = rotation_answer
+				#time.sleep(500)
+				#print ser.readline()
+
+		#answer1 = result1.get(timeout=1) #Shutting down the tag_search in a super ugly way.
+	except KeyboardInterrupt:
+		print("Caught Keyboard interrupt")
+		pool.terminate()
+		pool.join()
+		clf.close()
+		ser.write('1')
+		ser.close()	
+		GPIO.cleanup()
+	else: 
+		print "Quitting normally"
+		pool.close()
+		pool.join()
+		clf.close()
+		ser.write('1')
+		ser.close()
+		GPIO.cleanup()

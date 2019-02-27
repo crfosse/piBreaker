08037ebca50a0477bcd027a04f81a1893211ba06
erikrm <erikrm@stud.ntnu.no>
2019-02-27 21:24:25 +0800

    Tested and working version of tcleaned code

--- RFID_storage/main.py
+++ RFID_storage/main.py
@@ -12,9 +12,17 @@ import atexit
 	'storage position': (possible values: 1, 2, 3, 4)
 """
 firebase = firebase.FirebaseApplication(dsn='https://rfid-storage.firebaseio.com', authentication=None)
+global button_pressed
 button_pressed = False
-atexit.register(on_exit)
 
+def on_exit(pool, clf, ser):
+	pool.terminate()
+	pool.join()
+	clf.close()
+	ser.write('1')
+	ser.close()	
+	GPIO.cleanup()
+	
 #Firebase functions:
 def store_item_by_nfc_tag(nfc_tag):
 	item_uid = str(nfc_tag)[12:]
@@ -31,32 +39,35 @@ def button_insert2storage(pin_number):
 		button_pressed  = True
 	
 def main():
+	global button_pressed
+	pool = Pool()
 	clf = nfc.ContactlessFrontend()
-	assert clf.open('tty:AMA0:pn532') is True
 	ser = serial.Serial('/dev/ttyACM0',9600) #Arduino communication
-	
+	atexit.register(lambda: on_exit(pool, clf, ser))
+
+	assert clf.open('tty:AMA0:pn532') is True
+
 	#Button interrupt:
 	GPIO.setmode(GPIO.BOARD) #Physical pin numbering
 	GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
 	GPIO.add_event_detect(16, GPIO.RISING, callback=button_insert2storage, bouncetime=3000)
 
 	#Async:
-	pool = Pool()
-	current_position = 0
-	previous_position = 1
+	current_position = 1
 
 	pool.apply_async(lambda: nfc_tag_listener(clf))
+
 	while True:
 		if button_pressed:
 			button_pressed = False
 			new_position = (current_position % 4) + 1  # (1->2), (2->3), (3->4), (4->1)
 			firebase.put('/', 'rotation position', new_position, connection=None)
-		
-		firebase_rotation_position = firebase.get('rotation position', name=None, connection=None)
-		if (previous_position != firebase_rotation_position):
-			ser.write(str(firebase_rotation_position))
-			previous_position = firebase_rotation_position
+
+		firebase_current_position = firebase.get('rotation position', name=None, connection=None)
+		if (current_position != firebase_current_position):
+			ser.write(str(firebase_current_position))
+			current_position = firebase_current_position
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 	main()
